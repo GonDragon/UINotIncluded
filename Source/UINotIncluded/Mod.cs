74831a2ab072d3e1e54d13f167e21fc08e5c7d0b
Gonzalo Romero <gondragon13@gmail.com>
2021-08-14 19:40:21 -0300

    initial commit

---
+++ Source/UINotIncluded/Mod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace UINotIncluded
+{
+    [StaticConstructorOnStartup]
+    public static class Mod
+    {
+        static Mod()
+        {
+            const string Id = "UINotIncluded";
+            const string Name = Id;
+            const string Version = "1.0.0";
+
+            Log.Message("UINotIncluded Initialized");
+        }
+    }
+}
