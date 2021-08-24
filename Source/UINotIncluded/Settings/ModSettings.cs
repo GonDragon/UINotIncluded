917f704f6333bf76def9ea11036f8fd5238f10c7
Gonzalo Romero <gondragon13@gmail.com>
2021-08-24 19:27:08 -0300

    Fixed collection saving.

--- Source/UINotIncluded/Settings/ModSettings.cs
+++ Source/UINotIncluded/Settings/ModSettings.cs
@@ -18,9 +18,9 @@ namespace UINotIncluded
         public static bool vanillaAnimals = false;
 
         public static List<String> hiddenDesignations = new List<String>();
-        public static List<String> leftDesignations = new List<String>();
-        public static List<String> mainDesignations = new List<String>();
-        public static List<String> rightDesignations = new List<String>();
+        public static List<String> leftDesignations = new List<string> { "Forbid", "Uninstall", "Allow", "Claim" };
+        public static List<String> mainDesignations = new List<string> { "Harvest", "Deconstruct", "Cancel", "Chop wood", "Mine" };
+        public static List<String> rightDesignations = new List<string> { "Strip", "Open", "Smooth surface", "Tame", "Haul things", "Slaughter", "Cut plants", "Hunt" };
 
         public static bool initializedDesignations = false;
         public static readonly List<String>[] designationConfigs = new List<String>[] { hiddenDesignations, leftDesignations, mainDesignations, rightDesignations };
@@ -28,42 +28,49 @@ namespace UINotIncluded
         public override void ExposeData()
         {
             Scribe_Values.Look(ref tabsOnTop, "tabsOnTop", false);
+            Scribe_Values.Look(ref altInspectActive, "altInspectActive", true);
             Scribe_Values.Look(ref dateFormat, "dateFormat", DateFormat.MMDDYYYY);
+            Scribe_Values.Look(ref vanillaArchitect, "vanillaArchitect", false);
+            Scribe_Values.Look(ref vanillaAnimals, "vanillaAnimals", false);
+            Scribe_Values.Look(ref initializedDesignations, "initializedDesignations", false);
+
+            Scribe_Collections.Look(ref hiddenDesignations, "hiddenDesignations",LookMode.Value);
+            Scribe_Collections.Look(ref leftDesignations, "leftDesignations", LookMode.Value);
+            Scribe_Collections.Look(ref mainDesignations, "mainDesignations", LookMode.Value);
+            Scribe_Collections.Look(ref rightDesignations, "rightDesignations", LookMode.Value);
+
             base.ExposeData();
         }
 
         public static void RestoreDesignationLists()
         {
+            SetDefaultList(Settings.DesignationConfig.hidden);
             SetDefaultList(Settings.DesignationConfig.left);
             SetDefaultList(Settings.DesignationConfig.main);
             SetDefaultList(Settings.DesignationConfig.right);
-            UINotIncludedSettings.hiddenDesignations.Clear();
+            UINotIncludedStatic.Log("DesignationConfigs default restored.");
         }
 
         private static void SetDefaultList(Settings.DesignationConfig designationList)
         {
-            List<String> orderedDesignationNames;
             switch (designationList)
             {
                 case Settings.DesignationConfig.hidden:
-
+                    hiddenDesignations = new List<string>();
                     return;
                 case Settings.DesignationConfig.left:
-                    orderedDesignationNames = new List<string> { "Forbid", "Uninstall", "Allow", "Claim" };
+                    leftDesignations = new List<string> { "Forbid", "Uninstall", "Allow", "Claim" };
                     break;
                 case Settings.DesignationConfig.main:
-                    orderedDesignationNames = new List<string> { "Harvest", "Deconstruct", "Cancel", "Chop wood", "Mine" };
+                    mainDesignations = new List<string> { "Harvest", "Deconstruct", "Cancel", "Chop wood", "Mine" };
                     break;
                 case Settings.DesignationConfig.right:
-                    orderedDesignationNames = new List<string> { "Strip", "Open", "Smooth surface", "Tame", "Haul things", "Slaughter", "Cut plants", "Hunt" };
+                    rightDesignations = new List<string> { "Strip", "Open", "Smooth surface", "Tame", "Haul things", "Slaughter", "Cut plants", "Hunt" };
                     break;
                 default:
                     throw new NotImplementedException();
 
             }
-            designationConfigs[(int)designationList].Clear();
-
-            designationConfigs[(int)designationList].AddRange(orderedDesignationNames);
         }
 
         public static List<Designator>[] GetDesignationConfigs()
@@ -106,14 +113,19 @@ namespace UINotIncluded
         public UINotIncludedMod(ModContentPack content) : base(content)
         {
             this.settings = GetSettings<UINotIncludedSettings>();
-            UINotIncludedSettings.RestoreDesignationLists();
-            if (!UINotIncludedSettings.initializedDesignations) { UINotIncludedSettings.RestoreDesignationLists(); UINotIncludedSettings.initializedDesignations = true; }
+            if (!UINotIncludedSettings.initializedDesignations) {
+                UINotIncludedStatic.Log("DesigationConfigs never initialized. Initializing.");
+                UINotIncludedSettings.RestoreDesignationLists();
+                UINotIncludedSettings.initializedDesignations = true;
+                settings.Write();
+            }
         }
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
             float columnWidth = inRect.width / 3;
-            Rect column1 = new Rect(inRect.x, inRect.y, columnWidth, inRect.height);
+            float heigth = (float)Math.Floor(inRect.height / 3);
+            Rect column1 = new Rect(inRect.x, inRect.y, columnWidth, heigth);
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(column1);
             listingStandard.CheckboxLabeled("UINotIncluded.Setting.tabsOnTop".Translate(), ref UINotIncludedSettings.tabsOnTop, "UINotIncluded.Setting.tabsOnTop.Description".Translate());
@@ -130,7 +142,22 @@ namespace UINotIncluded
                 Find.WindowStack.Add((Window)new FloatMenu(options));
             }
             listingStandard.End();
+            DoJobBarConfigurationWidget(new Rect(inRect.x, inRect.y + heigth, inRect.width, inRect.height - heigth));
             base.DoSettingsWindowContents(inRect);
+        }
+
+        public void DoJobBarConfigurationWidget(Rect rect)
+        {
+            float columnWidth = rect.width / 4;
+            float curY = rect.y;
+
+
+            Widgets.ButtonText(new Rect(rect.x,curY,rect.width,25f), "Reestore to default");
+            curY += 25f;
+            for(int i = 0; i < 4; i++)
+            {
+                Widgets.DrawAtlas(new Rect(rect.x + columnWidth * i, curY, columnWidth, rect.height - 25f).ContractedBy(3f), ContentFinder<Texture2D>.Get("GD/UI/ClockSCR"));
+            }
         }
 
         public override string SettingsCategory()
