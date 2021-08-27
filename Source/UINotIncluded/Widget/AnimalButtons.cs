e7ea9f93b327d7c913485f1c3c17cd30aeeadb71
Gonzalo Romero <gondragon13@gmail.com>
2021-08-26 22:45:41 -0300

    Constant Animal Button Font

--- Source/UINotIncluded/Widget/AnimalButtons.cs
+++ Source/UINotIncluded/Widget/AnimalButtons.cs
@@ -19,6 +19,7 @@ namespace UINotIncluded.Widget
         {
             row.Init(posX, posY, UIDirection.RightThenDown);
 
+            Text.Font = GameFont.Tiny;
             if (row.ButtonText("Animals", fixedWidth: (float)Math.Floor(width / 2))) Find.MainTabsRoot.ToggleTab(DefDatabase<MainButtonDef>.GetNamed("Animals"));
             if (row.ButtonText("Wildlife", fixedWidth: (float)Math.Floor(width / 2))) Find.MainTabsRoot.ToggleTab(DefDatabase<MainButtonDef>.GetNamed("Wildlife"));
         }
