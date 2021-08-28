750f78b79cfeff906ce707a7420e29de8b13deaf
Gonzalo Romero <gondragon13@gmail.com>
2021-08-28 18:50:21 -0300

    Learning Helper Posicion

---
+++ Source/UINotIncluded/Patches/WindowStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Verse;
+using UnityEngine;
+
+namespace UINotIncluded
+{
+    [HarmonyPatch(typeof(Verse.WindowStack), "ImmediateWindow")]
+    class WindowStackPatch
+    {
+        public static void Prefix(int ID, ref Rect rect)
+        {
+            if (ID == 76136312)
+            {
+                rect.y += 75f;
+                if(UINotIncludedSettings.tabsOnTop) rect.y += UIManager.ExtendedBarHeight;
+            }
+        }
+    }
+}
