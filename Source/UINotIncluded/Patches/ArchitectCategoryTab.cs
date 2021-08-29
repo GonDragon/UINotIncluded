d73eaf87eb883456d7fdd1bd1b0ad5488046ecc9
Gonzalo Romero <gondragon13@gmail.com>
2021-08-28 22:37:56 -0300

    InfoBox react to TabsOnTop

---
+++ Source/UINotIncluded/Patches/ArchitectCategoryTab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+using RimWorld;
+
+namespace UINotIncluded
+{
+    [HarmonyPatch(typeof(ArchitectCategoryTab), "DoInfoBox")]
+    public class ArchitectCategoryTabPatches
+    {
+        public static void Prefix(ref Rect infoRect)
+        {
+            if (UINotIncludedSettings.tabsOnTop) infoRect.y += UIManager.ExtendedBarHeight;
+        }
+    }
+}
