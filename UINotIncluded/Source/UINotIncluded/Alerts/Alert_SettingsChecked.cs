38cbf750f60b757dd2cdf9e999b11f51e6c12b1d
Gonzalo Romero <gondragon13@gmail.com>
2021-10-18 17:16:11 -0300

    First time alert and extra check on deleted button defs.

---
+++ UINotIncluded/Source/UINotIncluded/Alerts/Alert_SettingsChecked.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+
+namespace UINotIncluded.Alerts
+{
+    class Alert_SettingsChecked : Alert
+    {
+        public Alert_SettingsChecked()
+        {
+            this.defaultLabel = (string)"UINotIncluded.Alerts.SettingsChecked.Label".Translate();
+            this.defaultExplanation = (string)"UINotIncluded.Alerts.SettingsChecked.Description".Translate();
+            this.defaultPriority = AlertPriority.High;
+        }
+        public override AlertReport GetReport()
+        {
+            return (AlertReport)(!Settings.settingsChecked);
+        }
+    }
+}
