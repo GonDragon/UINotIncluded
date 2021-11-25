3fa030ccbfdb1771f7428cf9b1dcbb10cd827bf7
Gonzalo Romero <gondragon13@gmail.com>
2021-11-25 16:28:20 -0300

    Pushed up Harmony patch priority

--- UINotIncluded/Source/UINotIncluded/Patches/ResourceReadout.cs
+++ UINotIncluded/Source/UINotIncluded/Patches/ResourceReadout.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 namespace UINotIncluded
 {
-    [HarmonyPatch(typeof(ResourceReadout), "DoReadoutSimple"), HarmonyPriority(Priority.High)]
+    [HarmonyPatch(typeof(ResourceReadout), "DoReadoutSimple")]
     internal class DoReadoutSimplePatch
     {
+        [HarmonyPriority(Priority.First)]
         public static void Prefix(ref Rect rect)
         {
             rect.y += UIManager.ResourceGap;
         }
     }
 
-    [HarmonyPatch(typeof(ResourceReadout), "DoReadoutCategorized"), HarmonyPriority(Priority.High)]
+    [HarmonyPatch(typeof(ResourceReadout), "DoReadoutCategorized")]
     internal class DoReadoutCategorizedPatch
     {
+        [HarmonyPriority(Priority.First)]
         public static void Prefix(ref Rect rect)
         {
             rect.y += UIManager.ResourceGap;
