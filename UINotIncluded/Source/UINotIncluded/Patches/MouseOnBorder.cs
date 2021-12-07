40ec6a64c4ed63a3ba606454745f930cd558afa7
Gonzalo Romero <gondragon13@gmail.com>
2021-12-07 19:01:49 -0300

    Finished transpiler for CameraDriver

--- UINotIncluded/Source/UINotIncluded/Patches/MouseOnBorder.cs
+++ UINotIncluded/Source/UINotIncluded/Patches/MouseOnBorder.cs
@@ -23,13 +23,20 @@ namespace UINotIncluded.Patches
             OpCode[] target = new OpCode[] { OpCodes.Ldloca_S, OpCodes.Ldflda, OpCodes.Dup, OpCodes.Ldind_R4, OpCodes.Ldarg_0, OpCodes.Ldfld, OpCodes.Ldfld, OpCodes.Add, OpCodes.Stind_R4 };
             int matches = 0;
 
-            //var vector2 = gen.DeclareLocal(typeof(Vector2));
-            //vector2.SetLocalSymInfo("vector2");
+            int numbermatches = 0;
+            bool numberInjected = false;
 
             var Field_mouseTouchingScreenBottomEdgeStartTime = AccessTools.Field(typeof(CameraDriver), "mouseTouchingScreenBottomEdgeStartTime");
 
             foreach (var code in instructions)
             {
+                if (code.opcode == OpCodes.Ldc_R4 && (float)code.operand == 20f) numbermatches++;
+                if (numbermatches == 3 && !numberInjected)
+                {
+                    code.operand = 6f;
+                    numberInjected = true;
+                }
+
                 if (!detected && code.opcode == OpCodes.Ldloca_S) detected = true;
                 if (detected)
                 {
@@ -90,6 +97,11 @@ namespace UINotIncluded.Patches
 
         public static void MoveIfTimer(float dollyRateScreenEdge,ref Vector2 vector2, ref float mouseTouchingScreenBottomEdgeStartTime)
         {
+            if (!Settings.TabsOnTop)
+            {
+                vector2.y += dollyRateScreenEdge;
+                return;
+            }
             if (mouseTouchingScreenBottomEdgeStartTime < 0.0)
             {
                 mouseTouchingScreenBottomEdgeStartTime = Time.realtimeSinceStartup;
